62bc6bcf8ab6e5759d0a08f312e789944e67ccb1
amoonfana <amoonfana@qq.com>
2016-08-11 19:55:54 +0800

    add Coroutine

---
+++ Coroutine/Coroutine/Coroutine.cs
@@ -0,0 +1,64 @@
+﻿//Gong Xueyuan
+//2016/8/11
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Coroutine
+{
+    public static class Coroutine
+    {
+        static List<IEnumerator> routines = new List<IEnumerator>();
+
+        public static int Count
+        {
+            get { return routines.Count; }
+        }
+
+        public static void Start(IEnumerator routine)
+        {
+            routines.Add(routine);
+        }
+
+        public static void StopCoroutine(IEnumerator routine)
+        {
+            routines.Remove(routine);
+        }
+
+        public static void StopAll()
+        {
+            routines.Clear();
+        }
+
+        public static void Update()
+        {
+            for (int i = 0; i < routines.Count; ++i)
+            {
+                if (routines[i].Current is IEnumerator)
+                {
+                    if (RecursiveMoveNext(routines[i].Current as IEnumerator))
+                    {
+                        continue;
+                    }
+                }
+
+                if (!routines[i].MoveNext())
+                {
+                    routines.RemoveAt(i--);
+                }
+            }
+        }
+
+        static bool RecursiveMoveNext(IEnumerator routine)
+        {
+            if (routine.Current is IEnumerator)
+            {
+                if (RecursiveMoveNext(routine.Current as IEnumerator))
+                {
+                    return true;
+                }
+            }
+
+            return routine.MoveNext();
+        }
+    }
+}
---
+++ Coroutine/Coroutine/Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Coroutine
+{
+    class Test
+    {
+        #region test coroutine
+        static IEnumerator WaitForSeconds(double seconds)
+        {
+            DateTime startedWaiting = DateTime.UtcNow;
+            while ((DateTime.UtcNow - startedWaiting).TotalSeconds < seconds)
+            {
+                yield return true;
+            }
+        }
+
+        static IEnumerator testYield()
+        {
+            Console.WriteLine("NestedCoroutine1, wait 1 sec (literally)");
+            yield return WaitForSeconds(1);
+            Console.WriteLine("Now wait 2 more.");
+            yield return WaitForSeconds(2);
+            Console.WriteLine("All done!");
+        }
+
+        static void Main(string[] args)
+        {
+            Coroutine.Start(testYield());
+
+            while (Coroutine.Count > 0)
+            {
+                Coroutine.Update();
+            }
+
+            Console.Read();
+        }
+        #endregion
+    }
+}
